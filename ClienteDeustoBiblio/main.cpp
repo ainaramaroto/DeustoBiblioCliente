8e6ab6b917a00e687cd9a2e793d25825d10fc120
naroaazcona <naroa.azcona@opendeusto.es>
2025-05-30 10:52:23 +0200

    main del Cliente

--- ClienteDeustoBiblio/main.cpp
+++ ClienteDeustoBiblio/main.cpp
@@ -52,8 +52,9 @@ int main(int argc, char *argv[]) {
 			ntohs(server.sin_port));
 
 	/*EMPIEZA EL PROGRAMA DEL CLIENTE*/
-	char opcion, opcionAdminInicio, usuario[50], contrasenia[50];
-	int intentos = 3, resultado;
+	char opcion, opcionAdminInicio, usuario[50], contrasenia[50], dni[10], nombre[20], apellido[20], email[50], numeroTlf[10], direccion[50],
+		opcionClienteInicio, opcionClientePrincipal, titulo[100]																										 ;
+	int intentos = 3, resultado, encontrado;
 	do{
 		opcion =  menuPrincipal();
 		sprintf(sendBuff,"%c",opcion); //almacena opción
@@ -106,7 +107,214 @@ int main(int argc, char *argv[]) {
 				}
 			}while (opcionAdminInicio != '0');
 			break;
-		}
+		case '2':
+					do {
+						opcionClienteInicio = menuClienteInicio();
+						sprintf(sendBuff, "%c", opcionClienteInicio); //almacena opción
+						send(s, sendBuff, sizeof(sendBuff), 0); //enviar
+
+						//recv(s, recvBuff, sizeof(recvBuff), 0);  //recibir
+						//cout << recvBuff << endl;
+
+						switch (opcionClienteInicio) {
+						case '1':  //HECHO Y SE VISUALIZA
+							cout << "Introduce tu DNI: ";
+							cin >> dni;
+							cout << "Introduce tu contraseña: ";
+							cin >> contrasenia;
+							//VARIABLES DNI Y CONTRASEÑA MANDADAS
+							sprintf(sendBuff, "%s", dni); //almacena opción
+							send(s, sendBuff, sizeof(sendBuff), 0); //enviar
+							sprintf(sendBuff, "%s", contrasenia); //almacena opción
+							send(s, sendBuff, sizeof(sendBuff), 0); //enviar
+
+							recv(s, recvBuff, sizeof(recvBuff), 0);
+							cout << recvBuff << endl;
+
+							recv(s, recvBuff, sizeof(recvBuff), 0);
+							sscanf(recvBuff, "%d", &encontrado);
+							cout<<encontrado<<endl;
+							if (encontrado) {
+								do {
+									opcionClientePrincipal = menuPrincipalCliente();
+									sprintf(sendBuff, "%c", opcionClientePrincipal); //almacena opción
+									send(s, sendBuff, sizeof(sendBuff), 0); //enviar
+									//recv(s, recvBuff, sizeof(recvBuff), 0);  //recibir
+									//cout << recvBuff << endl;
+
+									switch (opcionClientePrincipal) {
+									case '1':
+										//VER PERFIL
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout << recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout << recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout << recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout << recvBuff;
+
+										break;
+							case '2':
+										recv(s, recvBuff, sizeof(recvBuff), 0); //VISUALIZAR VER LIBROS DISPONIBLES
+										cout << recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout << recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout << recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										int fin;
+										sscanf(recvBuff, "%d", &fin);
+
+										while (fin == 0) {
+											recv(s, recvBuff, sizeof(recvBuff), 0);
+											cout << recvBuff;
+											recv(s, recvBuff, sizeof(recvBuff), 0);
+											sscanf(recvBuff, "%d", &fin);
+
+										}
+
+										//IF(COUNT == 0)
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout << recvBuff;
+
+										break;
+									case '3':
+										//VISUALIZAR: RESERVAR LIBROS
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+
+										cout<<"Introduce el título del libro que quieres alquilar: ";
+										cin.ignore();
+										cin.getline(titulo, sizeof(titulo));
+
+										strcpy(sendBuff, titulo);
+										send(s, sendBuff, strlen(sendBuff)+1, 0);
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+
+										break;
+
+									case '4':
+										//VISUALIZAR DEVOLVER LIBRO
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+
+										cout<<"Introduce el titulo del libro que quieres devolver: ";
+										cin.ignore();
+										cin.getline(titulo, sizeof(titulo));
+										strcpy(sendBuff, titulo);
+										send(s, sendBuff, strlen(sendBuff)+1, 0);
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+
+										break;
+
+									case '5':
+										//VISUALIZA VER LIBROS RESERVADOS
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff),0);
+										cout<<recvBuff;
+
+										recv(s, recvBuff, sizeof(recvBuff),0);
+										cout<<recvBuff;
+
+										//recv(s, recvBuff, sizeof(recvBuff),0);
+										//cout<<recvBuff;
+
+										break;
+
+									case '0':
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+										break;
+
+									default://MIRAR POR QUE NO SALE ESTE MENSAJE
+										recv(s, recvBuff, sizeof(recvBuff), 0);
+										cout<<recvBuff;
+
+									}
+								} while (opcionClientePrincipal != '0');
+							}
+							break;
+					case '2':
+							printf("Introduce DNI:");
+							cin.ignore();
+							cin.getline(dni, sizeof(dni));
+							strcpy(sendBuff, dni);
+							send(s, sendBuff, strlen(sendBuff)+1, 0);
+
+							recv(s, recvBuff, sizeof(recvBuff), 0);
+							sscanf(recvBuff, "%d", &encontrado);
+
+							if(encontrado == 1){
+								printf("\033[1;32m El usuario con DNI %s ya está registrado.\033[0m\n", dni);
+								break;
+							}
+
+							printf("Introduce nombre:");
+							cin.getline(nombre, sizeof(nombre));
+							strcpy(sendBuff, nombre);
+							send(s, sendBuff, strlen(sendBuff)+1, 0);
+
+							printf("Introduce apellido: ");
+							cin.getline(apellido, sizeof(apellido));
+							strcpy(sendBuff, apellido);
+							send(s, sendBuff, strlen(sendBuff)+1, 0);
+
+							printf("Introduce email: ");
+							cin.getline(email, sizeof(email));
+							strcpy(sendBuff, email);
+							send(s, sendBuff, strlen(sendBuff)+1, 0);
+
+							printf("Introduce contraseña: ");
+							cin.getline(contrasenia, sizeof(contrasenia));
+							strcpy(sendBuff, contrasenia);
+							send(s, sendBuff, strlen(sendBuff)+1, 0);
+
+							printf("Introduce número de teléfono: ");
+							cin.getline(numeroTlf, sizeof(numeroTlf));
+							strcpy(sendBuff, numeroTlf);
+							send(s, sendBuff, strlen(sendBuff)+1, 0);
+
+							printf("Introduce dirección: ");
+							cin.getline(direccion, sizeof(direccion));
+							strcpy(sendBuff, direccion);
+							send(s, sendBuff, strlen(sendBuff)+1, 0);
+
+							recv(s, recvBuff, sizeof(recvBuff), 0);
+							cout<<recvBuff;
+							break;
+						}
+
+					} while (opcionClienteInicio != '0');
+					break;
+				}
 
 	}while(opcion!='0');
 
